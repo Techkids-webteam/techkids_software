1b11070012c87454c9577c70c92270a830108eb8
Pt <dracarys1312@gmail.com>
2016-11-30 17:59:20 +0700

    add our team page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -73,4 +73,44 @@
   }
 
   // Your custom JavaScript goes here
+  $('#mySelect').on('change', function(e) {
+	$('#myTab li a').eq($(this).val()).tab('show');
+  });
+	$('.responsive-slick').slick({
+	  dots: false,
+	  autoplay: true,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 6,
+	  responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+			// vertical: true,
+			// verticalSwiping: true
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
+	});
+	
 })();
--- app/server.js
+++ app/server.js
@@ -2,11 +2,14 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 app.use('/images/', express.static(__dirname + '/images/'));
+app.use('/img/', express.static(__dirname + '/images/'));
 app.use('/scripts/', express.static(__dirname + '/scripts/'));
 app.use('/styles/', express.static(__dirname + '/styles/'));
 app.use('/scripts/', express.static(__dirname + '/scripts/'));
 app.use('/fonts/', express.static(__dirname + '/fonts/'));
+app.use('/libs/', express.static(__dirname + '/libs/'));
 app.set('views', __dirname + '/views/');
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
@@ -19,6 +22,18 @@ app.get("/", function(req, res) {
 
 app.get("/service", function(req, res) {
   res.sendFile(__dirname + '/views/service.html');
+})
+
+app.get("/contact", function(req, res) {
+  res.sendFile(__dirname + '/views/contact.html');
+})
+
+app.get("/portfolio", function(req, res) {
+  res.sendFile(__dirname + '/views/portfolio.html');
+})
+
+app.get("/our-team", function(req, res) {
+  res.sendFile(__dirname + '/views/team.html');
 })
 
 var server = app.listen(8080, function() {
